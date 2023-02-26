c7e298a94205a1736b3e702a7385b66b5974c3f5
rnjsrbgud@naver.com <rnjsrbgud@naver.com>
2023-02-26 22:45:19 +0900

    로그아웃 구현

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,1 +1,20 @@
-import Head from 'next/head'
+import Layout from "@/components/Common/Layout";
+import Seo from "@/components/Seo";
+import { AppProps } from "next/app";
+import Head from "next/head";
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Layout>
+        <Seo
+          title="validation_form"
+          keywords={["인풋 확인"]}
+          descrption="이풋 확인창"
+        />
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
+};
+export default MyApp;
